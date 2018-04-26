c483371104b4139d758c5738ee53218b0fba7235
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2018-04-26 21:43:29 +0530

    Halfway done with this new feature. Tested so far.

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-function something {
+function aBigNewFeature() {
+console.log('Writing a lot of code.');
+	console.log('Halfway done with this new feature.');
+}
+
+function something() {
 	console.log('I\'m something');
 }
 console.log('hello world!');
 something();
+
+aBigNewFeature();
